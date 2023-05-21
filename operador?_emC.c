5b873dc27656b456ed606b9dc4e6d590e71fe72e
Jean <112350318+JeanSousa6@users.noreply.github.com>
2023-05-21 13:26:59 -0300

    Create operador?_emC.c

---
+++ operador?_emC.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+
+/*Exp1? Exp2 : Exp3;
+
+Se  Exp1 é V, Exp2 SENÃO Exp3*/
+
+int x = 10;
+int y = x > 9? 100 : 200;
+
+printf("O valor de y e: %d", y); //imprime 100
+
+
+}
